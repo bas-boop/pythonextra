3f7f28bff6e0bf6ee402c853532cd309f81c722b
Bas <30810@ma-web.nl>
2020-10-22 10:14:28 +0200

    ik van de klasgenoten map ding niet leuk, maar het is gelukt.

--- 02-FilesFolders/maak_mappen.py
+++ 02-FilesFolders/maak_mappen.py
@@ -6,8 +6,6 @@ tekst_regel = bestand.readline()
 
 while tekst_regel:
 
-
     tekst_regel = tekst_regel.strip()
     print(tekst_regel)
-    tekst_regel = bestand.readline()
-os.mkdir()
+    os.mkdir(tekst_regel)
