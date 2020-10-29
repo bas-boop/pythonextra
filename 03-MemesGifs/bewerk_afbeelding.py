a8daf77a58cb5d9b491c6909cca26cad1e57c280
Bas <30810@ma-web.nl>
2020-10-29 09:52:16 +0100

    opdracht 3

---
+++ 03-MemesGifs/bewerk_afbeelding.py
@@ -0,0 +1,15 @@
+from PIL import Image
+
+afbeelding = Image.open("deku.jpg")
+
+afbeelding.show()
+
+breedte = str(afbeelding.width)
+hoogte = str(afbeelding.height)
+
+helft_breedte = afbeelding.width// 2
+helft_hoogte = afbeelding.height// 2
+
+nieuwe_afmeting = (helft_breedte, helft_hoogte)
+kleinere_afbeelding = afbeelding.resize(nieuwe_afmeting)
+kleinere_afbeelding.save('deku_klein.jpg')
